e9d680b4d6c1105bf51b6d648609b5f135098f0e
AREVALO GONZALEZ <AREVALO GONZALEZ@DESKTOP-GS3DUMB>
2018-03-01 05:47:33 -0600

    interfaz Exponencial casi terminada

--- src/Analisis/Exponencial.java
+++ src/Analisis/Exponencial.java
@@ -16,6 +16,12 @@ public class Exponencial extends javax.swing.JFrame {
      */
     public Exponencial() {
         initComponents();
+         this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        txtValorReal.setEditable(false);
+        vista();
+        btnFin.setVisible(false);
+        txtaCon.setVisible(false);
     }
 
     /**
@@ -27,37 +33,140 @@ public class Exponencial extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        lbla = new javax.swing.JLabel();
+        lbl2 = new javax.swing.JLabel();
+        txtMetodo1 = new javax.swing.JTextField();
+        txtMetodo2 = new javax.swing.JTextField();
+        lblfx = new javax.swing.JLabel();
+        lbl1 = new javax.swing.JLabel();
+        btnCalc = new javax.swing.JButton();
+        txtValorReal = new javax.swing.JTextField();
         txtCifras = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
+        btnFin = new javax.swing.JButton();
+        lblEa = new javax.swing.JLabel();
+        lblEa1 = new javax.swing.JLabel();
+        txtEa1 = new javax.swing.JTextField();
+        txtEa2 = new javax.swing.JTextField();
+        btnConclusion = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtaCon = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setText("Ingrese la cantidad de cifras significativas");
+        lbla.setText("Ingrese la cantidad de cifras significativas");
+        getContentPane().add(lbla, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 232, 25));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(21, 21, 21)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 232, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(txtCifras, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(74, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtCifras, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(264, Short.MAX_VALUE))
-        );
+        lbl2.setText("Segundo Metodo");
+        getContentPane().add(lbl2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 180, -1, -1));
+        getContentPane().add(txtMetodo1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 130, 120, 30));
+        getContentPane().add(txtMetodo2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 170, 120, 30));
+
+        lblfx.setText("Valor Real de f(x)=e⁻⁵");
+        getContentPane().add(lblfx, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 60, 120, 20));
+
+        lbl1.setText("Primer Metodo");
+        getContentPane().add(lbl1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 130, -1, -1));
+
+        btnCalc.setText("Calcular con los metodos");
+        btnCalc.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCalcActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnCalc, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 90, 210, 30));
+        getContentPane().add(txtValorReal, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 60, 190, -1));
+
+        txtCifras.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtCifrasKeyTyped(evt);
+            }
+        });
+        getContentPane().add(txtCifras, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 10, 60, 30));
+
+        btnFin.setText("finalizar");
+        btnFin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnFinActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnFin, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 250, 120, 40));
+
+        lblEa.setText("Ea");
+        getContentPane().add(lblEa, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 180, -1, -1));
+
+        lblEa1.setText("Ea");
+        getContentPane().add(lblEa1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 140, -1, -1));
+        getContentPane().add(txtEa1, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 140, 140, -1));
+        getContentPane().add(txtEa2, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 180, 140, -1));
+
+        btnConclusion.setText("Concluisones");
+        btnConclusion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnConclusionActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnConclusion, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 240, 120, 50));
+
+        txtaCon.setEditable(false);
+        txtaCon.setColumns(20);
+        txtaCon.setRows(5);
+        txtaCon.setBorder(null);
+        jScrollPane1.setViewportView(txtaCon);
+
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, 390, 210));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void txtCifrasKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtCifrasKeyTyped
+        char c= evt.getKeyChar();
+        if(c<'0' || c>'9' ){
+            evt.consume();
+        }         // TODO add your handling code here:
+    }//GEN-LAST:event_txtCifrasKeyTyped
+
+    private void btnCalcActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCalcActionPerformed
+        if(!txtCifras.getText().equals("")){
+            txtMetodo1.setVisible(true);
+            txtMetodo2.setVisible(true);
+            lbl1.setVisible(true);
+            lbl2.setVisible(true);
+            btnConclusion.setVisible(true);
+            btnCalc.setVisible(false);
+            lblEa.setVisible(true);
+        lblEa1.setVisible(true);
+        txtEa1.setVisible(true);
+        txtEa2.setVisible(true);
+        }
+    }//GEN-LAST:event_btnCalcActionPerformed
+
+    private void btnConclusionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConclusionActionPerformed
+     btnFin.setVisible(true);
+    vista();
+    lbla.setVisible(false);
+    lblfx.setVisible(false);
+    txtCifras.setVisible(false);
+    txtValorReal.setVisible(false);
+    txtaCon.setVisible(true);
+    txtaCon.setText("hola");
+    }//GEN-LAST:event_btnConclusionActionPerformed
+
+    private void btnFinActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnFinActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_btnFinActionPerformed
+ 
+    public void vista(){
+         lbl1.setVisible(false);
+        lbl2.setVisible(false);
+        txtMetodo1.setVisible(false);
+        txtMetodo2.setVisible(false);
+        lblEa.setVisible(false);
+        lblEa1.setVisible(false);
+        txtEa1.setVisible(false);
+        txtEa2.setVisible(false);
+     btnConclusion.setVisible(false);
+    }
     /**
      * @param args the command line arguments
      */
@@ -94,7 +203,22 @@ public class Exponencial extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton btnCalc;
+    private javax.swing.JButton btnConclusion;
+    private javax.swing.JButton btnFin;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lbl1;
+    private javax.swing.JLabel lbl2;
+    private javax.swing.JLabel lblEa;
+    private javax.swing.JLabel lblEa1;
+    private javax.swing.JLabel lbla;
+    private javax.swing.JLabel lblfx;
     private javax.swing.JTextField txtCifras;
+    private javax.swing.JTextField txtEa1;
+    private javax.swing.JTextField txtEa2;
+    private javax.swing.JTextField txtMetodo1;
+    private javax.swing.JTextField txtMetodo2;
+    private javax.swing.JTextField txtValorReal;
+    private javax.swing.JTextArea txtaCon;
     // End of variables declaration//GEN-END:variables
 }
--- src/Analisis/Operaciones.java
+++ src/Analisis/Operaciones.java
@@ -6,8 +6,9 @@ public class Operaciones {
     int Factorial(int n){
      int factorial=1,cont=1;
      while (cont<=n){
-         factorial+=cont*factorial;
+         factorial*=cont;
          cont++;
+         System.out.println("Factorial "+factorial);
      }     
      return factorial;
     } 
@@ -21,7 +22,6 @@ public class Operaciones {
     
     String Algoritmo(double Es ,int decimal,double n){
       String res="";
-        System.out.println("Grado de libertad "+Es);
         double Ea=100,resultado=0,resultAnt;
       int iteracion=0;
       while(Math.abs(Ea)>Es){
@@ -30,20 +30,20 @@ public class Operaciones {
                  resultado=1;
                  break;
              case 1:
-                 resultAnt=redondearDecimales(resultado, decimal);
+                 resultAnt=redondearDecimales(resultado, decimal+1);
                  
-                 resultado=redondearDecimales(resultado -(Math.pow(n, iteracion*2))/(iteracion*n), decimal);
+                 resultado=redondearDecimales(resultado -(Math.pow(n, iteracion*2))/(iteracion*n), decimal+1);
                  
                  Ea=((resultado-resultAnt)/resultado)*100;
                  break;
              default :
-                 resultAnt=redondearDecimales(resultado, decimal);
+                 resultAnt=redondearDecimales(resultado, decimal+1);
                  if(iteracion%2==0){
                      resultado= resultado+(Math.pow(n, iteracion*2))/(Factorial(iteracion*2));
-                     resultado=redondearDecimales(resultado, decimal);
+                     resultado=redondearDecimales(resultado, decimal+1);
                  }else{
                      resultado=resultado-(Math.pow(n, iteracion*2))/(Factorial(iteracion*2));
-                     resultado=redondearDecimales(resultado, decimal);
+                     resultado=redondearDecimales(resultado, decimal+1);
                  }
                  Ea=((resultado-resultAnt)/resultado)*100;
                  break;
@@ -80,18 +80,14 @@ public class Operaciones {
             disc=((b^2)-4*a*c);
            //falta poner lo de la raiz
            raiz=Math.sqrt(disc);
-           System.out.println("sin"+raiz);
            raiz=redondearDecimales(raiz, decimal);
-           System.out.println("con"+raiz);
            if(cont==1){
                raiz1=(-b-raiz)/(2*a);
                raiz1=redondearDecimales(raiz1, decimal);
                return raiz1;
            }else{
                raiz2=(-b+raiz)/(2*a);
-               System.out.println("sinra2"+raiz2);
                raiz2=redondearDecimales(raiz2, decimal);
-               System.out.println("connra2"+raiz2);
                return raiz2;
            }
         }else{
@@ -104,7 +100,6 @@ public class Operaciones {
         resultado = n;
         if(resultado>0){
             parteEntera = Math.floor(resultado);
-        System.out.println("parte entera"+parteEntera);
         resultado=(resultado-parteEntera)*Math.pow(10, decimal);
         resultado=Math.round(resultado);
         resultado=(resultado/Math.pow(10, decimal))+parteEntera;
@@ -112,7 +107,6 @@ public class Operaciones {
         }else{
             resultado*=-1;
             parteEntera = Math.floor(resultado);
-        System.out.println("parte entera"+parteEntera);
         resultado=(resultado-parteEntera)*Math.pow(10, decimal);
         resultado=Math.round(resultado);
         resultado=(resultado/Math.pow(10, decimal))+parteEntera;
